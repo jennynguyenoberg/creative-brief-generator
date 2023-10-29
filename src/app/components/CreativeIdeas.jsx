2996d8b8dd48218fc09709b27f633be7176379b0
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-29 13:51:51 +0100

    Implement state variables for rendering out deadline and item item with button trigger

--- src/app/components/CreativeIdeas.jsx
+++ src/app/components/CreativeIdeas.jsx
@@ -5,13 +5,63 @@ import data from './assets/ideasData.json'
 export default function CreativeIdeas() {
   const industryOptions = Object.keys(data.industries)
   const [selectedIndustry, setSelectedIndustry] = useState('Technology')
-  const categoriesForSelectedIndustry = data.industries[selectedIndustry]
+  const [selectedCategory, setSelectedCategory] = useState(
+    data.industries.Technology[0].category,
+  )
+  const [showIdeas, setShowIdeas] = useState(false)
+  const [tempSelectedIndustry, setTempSelectedIndustry] = useState('Technology')
+  const [tempSelectedCategory, setTempSelectedCategory] = useState(
+    data.industries.Technology[0].category,
+  )
+  const [currentIdea, setCurrentIdea] = useState('')
+  const [selectedCategoryDeadline, setSelectedCategoryDeadline] = useState('')
+
+  const handleIndustryChange = (event) => {
+    setTempSelectedIndustry(event.target.value)
+  }
+
+  const handleCategoryChange = (event) => {
+    setTempSelectedCategory(event.target.value)
+  }
+
+  const showIdeasOnClick = () => {
+    setSelectedIndustry(tempSelectedIndustry)
+    setSelectedCategory(tempSelectedCategory)
+
+    const selectedIndustryData = data.industries[tempSelectedIndustry] // Use tempSelectedIndustry
+    let ideas = []
+    let categoryDeadline = ''
+    let selectedCategoryData = null
+
+    if (selectedIndustryData) {
+      selectedCategoryData = selectedIndustryData.find(
+        (item) => item.category === tempSelectedCategory, // Use tempSelectedCategory
+      )
+      if (selectedCategoryData) {
+        ideas = selectedCategoryData.ideas
+        categoryDeadline = selectedCategoryData.deadline
+      }
+    }
+
+    setSelectedCategoryDeadline(categoryDeadline)
+
+    if (selectedCategoryData && ideas.length > 0) {
+      const randomIndex = Math.floor(Math.random() * ideas.length)
+      setCurrentIdea(ideas[randomIndex])
+    }
+
+    setShowIdeas(true)
+  }
 
   return (
     <div>
       <div>
-        <label htmlFor="industrySelect">Select an Industry:</label>
-        <select id="industrySelect" value="Technology">
+        <label htmlFor='industrySelect'>Select an Industry:</label>
+        <select
+          id='industrySelect'
+          value={tempSelectedIndustry}
+          onChange={handleIndustryChange}
+        >
           {industryOptions.map((industry) => (
             <option key={industry} value={industry}>
               {industry}
@@ -19,19 +69,35 @@ export default function CreativeIdeas() {
           ))}
         </select>
       </div>
+
       <div>
-        <label htmlFor="categorySelect">Select a Category:</label>
-        <select id="categorySelect" value="Logo Design">
-          {categoriesForSelectedIndustry.map((category) => (
-            <option key={category.category} value={category.category}>
-              {category.category}
+        <label htmlFor='categorySelect'>Select a Category:</label>
+        <select
+          id='categorySelect'
+          value={tempSelectedCategory}
+          onChange={handleCategoryChange}
+        >
+          {data.industries[tempSelectedIndustry].map((item) => (
+            <option key={item.category} value={item.category}>
+              {item.category}
             </option>
           ))}
         </select>
       </div>
+
       <div>
-        <button>Show Ideas</button>
+        <button onClick={showIdeasOnClick}>Show Ideas</button>
       </div>
+
+      {showIdeas && (
+        <div>
+          <h2>
+            Ideas for {selectedIndustry} - {tempSelectedCategory}{' '}
+          </h2>
+          <p>Deadline: {selectedCategoryDeadline}</p>
+          <p>Idea: {currentIdea}</p>
+        </div>
+      )}
     </div>
   )
 }
