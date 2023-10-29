3feccc69933f058b3d229d7d74450188b81ce95c
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-29 12:34:17 +0100

    Add two select element tags with options for Industry and Category

--- src/app/components/CreativeIdeas.jsx
+++ src/app/components/CreativeIdeas.jsx
@@ -1,5 +1,31 @@
-export default function CreativeIdeas() { 
+export default function CreativeIdeas() {
   return (
-    <div>Contents here</div>
-  )
+    <div>
+      <div>
+        <label htmlFor="industrySelect">Select an Industry:</label>
+        <select id="industrySelect" value="Technology">
+          <option value="Technology">Technology</option>
+          <option value="Food">Food</option>
+          <option value="Fashion">Fashion</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Sports">Sports</option>
+          <option value="Education">Education</option>
+          <option value="Travel">Travel</option>
+          <option value="Random">Random</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="categorySelect">Select a Category:</label>
+        <select id="categorySelect" value="Technology">
+          <option value="Logo Design">Logo Design</option>
+          <option value="Website">Website</option>
+          <option value="Brand Identity">Brand Identity</option>
+          <option value="Illustration">Illustration</option>
+        </select>
+      </div>
+      <div>
+        <button>Show Ideas</button>
+      </div>
+    </div>
+  );
 }
