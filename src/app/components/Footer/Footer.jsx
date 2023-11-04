5f3c3b51dd5e70a897e7531e5bab01547c580b4f
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-11-04 15:30:08 +0100

    Add link w/ hover state

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from './footer.module.css'
 
 export default function Footer() {
@@ -8,28 +9,26 @@ export default function Footer() {
           <h2>Ideaspire</h2>
           <span className={styles.footerCopy}>Fueling Your Creative Juice</span>
         </div>
-        <div>
-          Design and Development by Jenny Nguyen Öberg © 2023 All rights
-          reserved
+        <div className={`${styles.social} custom-link`}>Design and Development by{' '}
+          <Link href={'https://www.jennynguyenoberg.com/'} target="_blank" rel="noopener noreferrer">Jenny Nguyen Öberg</Link>
+          {' '}© 2023 All rights reserved
         </div>
       </div>
       <div className={styles.midddleColumn}>
         <h2>Social</h2>
-        <div>
+        <div className={`${styles.social} custom-link`}>
           <ul className={styles.social}>
-            <li>LinkedIn</li>
-            <li>Instagram</li>
-            <li>Dribbble</li>
-            <li>Behance</li>
+            <li><Link href={'https://www.linkedin.com/in/jennynguyenoberg'} target="_blank" rel="noopener noreferrer">LinkedIn</Link></li>
+            <li><Link href={'https://www.instagram.com/jennynguyenoberg/'} target="_blank" rel="noopener noreferrer">Instagram</Link></li>
+            <li><Link href={'https://github.com/jennynguyenoberg'} target="_blank" rel="noopener noreferrer">Github</Link></li>
+            <li><Link href={'https://www.behance.net/jennynguyenoberg'} target="_blank" rel="noopener noreferrer">Behance</Link></li>
           </ul>
         </div>
       </div>
       <div className={styles.rightColumn}>
         <div>
           <h2>Newsletter</h2>
-          <span className={styles.footerCopy}>
-            Be up to date with new ideas
-          </span>
+          <span className={styles.footerCopy}>Be up to date with new ideas</span>
         </div>
         <div className={styles.input}>
           <input id="subscribe" placeholder="Type your email" disabled={true} />
--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
+import Link from 'next/link'
 import styles from './navbar.module.css'
 
 export default function Navbar() {
   return (
     <>
-      <div className={styles.logo}>
-        <h2>Ideaspire</h2>
-      </div>
-      <div className={styles.menu}>
-        <h2>Menu</h2>
-      </div>
+        <div className={styles.logo}>
+          <div className={`${styles.social} custom-link`}>
+          <Link href={'/'} rel="noopener noreferrer">
+            <h2>Ideaspire</h2>
+          </Link>
+          </div>
+        </div>
+        <div className={styles.menu}>
+          <h2>Menu</h2>
+        </div>
     </>
   )
 }
