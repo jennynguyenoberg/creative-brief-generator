d63789fe835e8ed3596c162d69b26f940c7a7ec0
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-11-04 15:30:47 +0100

    Run Prettier

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -9,26 +9,67 @@ export default function Footer() {
           <h2>Ideaspire</h2>
           <span className={styles.footerCopy}>Fueling Your Creative Juice</span>
         </div>
-        <div className={`${styles.social} custom-link`}>Design and Development by{' '}
-          <Link href={'https://www.jennynguyenoberg.com/'} target="_blank" rel="noopener noreferrer">Jenny Nguyen Öberg</Link>
-          {' '}© 2023 All rights reserved
+        <div className={`${styles.social} custom-link`}>
+          Design and Development by{' '}
+          <Link
+            href={'https://www.jennynguyenoberg.com/'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jenny Nguyen Öberg
+          </Link>{' '}
+          © 2023 All rights reserved
         </div>
       </div>
       <div className={styles.midddleColumn}>
         <h2>Social</h2>
         <div className={`${styles.social} custom-link`}>
           <ul className={styles.social}>
-            <li><Link href={'https://www.linkedin.com/in/jennynguyenoberg'} target="_blank" rel="noopener noreferrer">LinkedIn</Link></li>
-            <li><Link href={'https://www.instagram.com/jennynguyenoberg/'} target="_blank" rel="noopener noreferrer">Instagram</Link></li>
-            <li><Link href={'https://github.com/jennynguyenoberg'} target="_blank" rel="noopener noreferrer">Github</Link></li>
-            <li><Link href={'https://www.behance.net/jennynguyenoberg'} target="_blank" rel="noopener noreferrer">Behance</Link></li>
+            <li>
+              <Link
+                href={'https://www.linkedin.com/in/jennynguyenoberg'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={'https://www.instagram.com/jennynguyenoberg/'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Instagram
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={'https://github.com/jennynguyenoberg'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={'https://www.behance.net/jennynguyenoberg'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Behance
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
       <div className={styles.rightColumn}>
         <div>
           <h2>Newsletter</h2>
-          <span className={styles.footerCopy}>Be up to date with new ideas</span>
+          <span className={styles.footerCopy}>
+            Be up to date with new ideas
+          </span>
         </div>
         <div className={styles.input}>
           <input id="subscribe" placeholder="Type your email" disabled={true} />
--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -4,16 +4,16 @@ import styles from './navbar.module.css'
 export default function Navbar() {
   return (
     <>
-        <div className={styles.logo}>
-          <div className={`${styles.social} custom-link`}>
+      <div className={styles.logo}>
+        <div className={`${styles.social} custom-link`}>
           <Link href={'/'} rel="noopener noreferrer">
             <h2>Ideaspire</h2>
           </Link>
-          </div>
         </div>
-        <div className={styles.menu}>
-          <h2>Menu</h2>
-        </div>
+      </div>
+      <div className={styles.menu}>
+        <h2>Menu</h2>
+      </div>
     </>
   )
 }
